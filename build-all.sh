ac1939188b2584dde92892041ff688c706a99d37
Stephen Heywood <stephen@ii.coop>
2024-08-08 11:04:01 +1200

    chore: determine flatpak dependencies

--- build-all.sh
+++ build-all.sh
@@ -27,6 +27,41 @@ else
 fi
 echo " ================================================================================== "
 
+// Ref: https://github.com/ublue-os/bluefin/blob/main/.github/workflows/reusable-build-iso.yml#L118
+echo "Determine Flatpak Dependencies"
+IMAGE="${REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+# Make temp space
+TEMP_FLATPAK_INSTALL_DIR=$(mktemp -d -p /tmp/flatpak.XXX)
+
+# Get list of refs from directory
+FLATPAK_REFS_DIR_LIST=$(cat aurora/flatpaks | tr '\n' ' ' )
+
+# Generate install script
+cat << EOF > ${TEMP_FLATPAK_INSTALL_DIR}/script.sh
+cat /temp_flatpak_install_dir/script.sh
+mkdir -p /flatpak/flatpak /flatpak/triggers
+mkdir /var/tmp || true
+chmod -R 1777 /var/tmp
+flatpak config --system --set languages "*"
+flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install --system -y ${FLATPAK_REFS_DIR_LIST}
+ostree refs --repo=\${FLATPAK_SYSTEM_DIR}/repo | grep '^deploy/' | grep -v 'org\.freedesktop\.Platform\.openh264' | sed 's/^deploy\///g' > /output/flatpaks_with_deps
+EOF
+
+docker run --rm --privileged \
+  --entrypoint bash \
+  -e FLATPAK_SYSTEM_DIR=/flatpak/flatpak \
+  -e FLATPAK_TRIGGERSDIR=/flatpak/triggers \
+  --volume ${FLATPAK_REFS_DIR}:/output \
+  --volume ${TEMP_FLATPAK_INSTALL_DIR}:/temp_flatpak_install_dir \
+  ${IMAGE} /temp_flatpak_install_dir/script.sh
+
+echo " ================================================================================== "
+
+cat ${FLATPAK_REFS_DIR}/flatpaks_with_deps
+FLATPAK_LIST=$(cat ${FLATPAK_REFS_DIR}/flatpaks_with_deps | tr '\n' ' ' )
+
 ISO_TMPDIR=$(mktemp -d /tmp/iso-$IMAGE_TAG-XXXX)
 echo "Build artifacts: $ISO_TMPDIR"
 cd $ISO_TMPDIR
