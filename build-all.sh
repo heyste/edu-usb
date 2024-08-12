7fbb003d5fa64944d98ad98d60c014ce3d31e69c
Stephen Heywood <stephen@ii.coop>
2024-08-12 16:53:21 +1200

    chore: use aurora 40 & latest build container

--- build-all.sh
+++ build-all.sh
@@ -17,37 +17,30 @@ echo " =========================================================================
 if [[ $IMAGE_BUILD == 'true' ]] ; then
   cd aurora
   podman build -t ${REGISTRY}/${IMAGE_NAME}:$IMAGE_TAG -f Containerfile .
+  podman images | head -1
+  podman images | grep $IMAGE_TAG
+  echo "Pushing ${REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}..."
+  podman push "${REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
   cd -
 else
   echo "Skipping image build. IMAGE_BULID: ${IMAGE_BUILD}"
 fi
 echo " ================================================================================== "
 
-podman images | head -1
-podman images | grep $IMAGE_TAG
-echo "Pushing ${REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}..."
-podman push "${REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
-echo " ================================================================================== "
-
 ISO_TMPDIR=$(mktemp -d /tmp/iso-$IMAGE_TAG-XXXX)
 echo "Build artifacts: $ISO_TMPDIR"
 cd $ISO_TMPDIR
 
-# FLATPAK_SONIC_PI="app/net.sonic_pi.SonicPi/x86_64/stable runtime/org.kde.Platform/x86_64/5.15-23.08"
-# FLATPAK_ECLIPSE="app/org.eclipse.Java/x86_64/stable runtime/org.gnome.Platform/x86_64/45"
-
 echo " ================================================================================== "
 
 sudo podman run --rm --privileged \
-                --volume .:/github/workspace/build \
-		docker.io/heyste/build-container-installer:latest \
-		VERSION=39  \
+		--volume .:/build-container-installer/build \
+		ghcr.io/jasonn3/build-container-installer:v1.2.2 \
+		VERSION=40  \
 		IMAGE_REPO=${REGISTRY} \
 		IMAGE_NAME=${IMAGE_NAME} \
 		IMAGE_TAG=${IMAGE_TAG} \
-		VARIANT=Server
-#		FLATPAK_REMOTE_REFS="$FLATPAK_ECLIPSE $FLATPAK_SONIC_PI" \
-#		FLATPAK_REMOTE_URL="https://flathub.org/repo/flathub.flatpakrepo"
+		VARIANT=Kinoite
 
 echo " ================================================================================== "
 echo "Build artifacts: $ISO_TMPDIR"
